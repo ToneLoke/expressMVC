eaec47bc4932918c46c94cc20be589c9adc4741c
toneloke <david.guerrero87@live.com>
2016-03-31 15:04:34 -0600

    added bower components

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors())
 app.use('/api/v1', apiRoutes)
-app.use(express.static(path.join(__dirname, './views')))
+app.use(express.static(path.join(__dirname, './public')))
 // http://localhost:3000/api/v1/cars
 app.listen(port, function () {
   console.log('Server running on port: ', port)
